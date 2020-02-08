601b9a772079c40dac895cb311f53d5051c80f15
Miley Wright <dishaliekwright@yahoo.com>
2020-02-07 22:02:33 -0500

    GET request for /resources

--- Routes/projectRoutes.js
+++ Routes/projectRoutes.js
@@ -5,7 +5,16 @@ const db = require('./projectModal');
 const router = express.Router();
 
 //GET Retrieving a list of resources /project/resources 
-
+router.get('/resources', (req,res) => {
+    db.getResources()
+        .then(resources => {
+            res.status(200).json({resources})
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'Could not retrieve a list of resources'})
+        })
+})
 
 //POST  Adding Resources /project/id/resources
 
